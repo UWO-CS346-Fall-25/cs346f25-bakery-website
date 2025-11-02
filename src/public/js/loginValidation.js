783449996d81d20097dbf7b959362946e629aeed
calebwisneski-ops <caleb.wisneski@gmail.com>
2025-11-02 08:51:59 -0600

    Added error messages and fixed validation for the login form

--- src/public/js/loginValidation.js
+++ src/public/js/loginValidation.js
@@ -1,10 +1,17 @@
 let LoginForm = document.querySelector("#login-form");
 LoginForm.addEventListener("submit", checkFields);
 
-function checkForm(event) {
+function checkFields(event) {
     let usernameInput = document.querySelector("#username");
     let passwordInput = document.querySelector("#password");
-    if (usernameInput.value.trim().length == 0 || passwordInput.value.trim().length == 0) {
+    let usernameError = document.querySelector("#username-error");
+    let passwordError = document.querySelector("#password-error");
+    if (usernameInput.value.trim().length == 0) {
+        usernameError.textContent = "Username is required";
+        event.preventDefault();
+    }
+    if(passwordInput.value.trim().length == 0){
+        passwordError.textContent = "Password is required";
         event.preventDefault();
     }
 }
