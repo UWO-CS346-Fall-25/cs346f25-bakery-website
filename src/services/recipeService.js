433a3029f288c7c4243eff9f85fc9fa71fae5a95
calebwisneski-ops <caleb.wisneski@gmail.com>
2025-11-23 16:02:14 -0600

    Feature/week12 layout (#6)
    * Implemented a dessert recipe of the day, calling the spoonacular free API
    
    * Deleted debug statements
    
    * Corrected footer at bottom of recipe of day page

---
+++ src/services/recipeService.js
@@ -0,0 +1,34 @@
+require("dotenv").config();
+
+const API_KEY = process.env.SPOONACULAR_API_KEY;
+
+let cachedRecipe = null;
+let cachedDate = null;
+
+async function fetchRandomBakingRecipe() {
+    const today = new Date().toISOString().split("T")[0];
+
+  // If recipe was already called today return it
+  if (cachedRecipe && cachedDate === today) {
+    return cachedRecipe;
+  }
+
+  const url = `https://api.spoonacular.com/recipes/random?apiKey=${API_KEY}&tags=dessert`;
+
+  const res = await fetch(url);
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch recipe from Spoonacular");
+  }
+
+  const data = await res.json();
+
+  const recipe = data.recipes[0];
+
+  cachedRecipe = recipe;
+  cachedDate = today;
+  
+  return recipe; // Spoonacular returns an array
+}
+
+module.exports = { fetchRandomBakingRecipe };
